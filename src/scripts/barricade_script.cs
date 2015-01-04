8c6a112b2c29a6054bfa569c12c43edaee43b57e
Pandaka <tristancailleau@hotmail.fr>
2015-01-04 18:53:22 +0100

    src
    mise à jour

---
+++ src/scripts/barricade_script.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barricade_script : MonoBehaviour {
+
+	public int vie = 100;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ src/scripts/player_script.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_script : MonoBehaviour {
+	public GUISkin skin = null;
+
+	public static int billet = 1000;
+
+	void OnGUI () {
+		GUI.skin = skin;
+
+		GUI.Label(new Rect(0,0,400,200), "Billet :" + billet);
+	}
+
+}
