5bada277f8587800ab49c7cdb210a9f8c84e98a6
Pandaka <tristancailleau@hotmail.fr>
2015-01-27 11:30:08 +0100

    ajout alpha

---
+++ src/projet/Assets/Scripts/player_script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_script : MonoBehaviour {
+	public GUISkin skin = null;
+
+	public static int billet = 1000;
+
+	public static int nb_mob_max=30;
+
+	public static int nb_mob=0;
+
+	void OnGUI () {
+		GUI.skin = skin;
+
+		GUI.Label(new Rect(0,0,400,200), "Billet :" + billet);
+	}
+
+}
